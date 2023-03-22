b2b664bce04827fe59a72f33da0d1bbc21319eb6
Prince <veluthedathuprince@gmail.com>
2023-03-22 09:10:09 +0100

    Inital commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const express = require('express');
+const http = require('http')
+
+
+const app = express();
+const server = http.createServer(app)
+//Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+const PORT = 3000 || process.env.PORT;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
